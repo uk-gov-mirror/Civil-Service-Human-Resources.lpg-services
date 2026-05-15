d1b82ec84d9508983e657545dc860c20b9433135
jamie <jamie.lovell@cabinetoffice.gov.uk>
2026-05-15 08:56:58 +0100

    Fix add to learning plan

--- src/ui/controllers/courseCatalogue/index.ts
+++ src/ui/controllers/courseCatalogue/index.ts
@@ -1,5 +1,6 @@
 import {plainToInstance} from 'class-transformer'
 import * as express from 'express'
+import {LPG_UI_SERVER} from '../../../lib/config'
 import * as cslService from '../../../lib/service/cslService/cslServiceClient'
 import {getPagination} from '../../../lib/utils/search'
 import {generateNotificationBanner} from '../home'
@@ -8,18 +9,23 @@ import {CataloguePageParams} from './model/cataloguePageParams'
 const aToZRegex = new RegExp('[a-zA-Z]')
 
 export async function addToPlan(req: express.Request, res: express.Response) {
-	const ref = req.query.ref
-
-	let redirectTo = '/course-catalogue/profile-preferences'
+	const referer = req.headers.referer
 	const courseId = req.params.courseId
-	switch (ref) {
-		case 'home':
-		case 'search':
-			redirectTo = '/'
-			break
-		case 'course':
-			redirectTo = `/courses/${courseId}`
-			break
+	let redirectTo = '/course-catalogue'
+	if (referer && referer.startsWith(`${LPG_UI_SERVER}/course-catalogue/`)) {
+		redirectTo = referer.replace(LPG_UI_SERVER, '')
+	} else {
+		const ref = req.query.ref
+
+		switch (ref) {
+			case 'home':
+			case 'search':
+				redirectTo = '/'
+				break
+			case 'course':
+				redirectTo = `/courses/${courseId}`
+				break
+		}
 	}
 	const resp = await cslService.addCourseToLearningPlan(req.params.courseId, req.user)
 
@@ -40,13 +46,16 @@ export async function renderAtoZ(req: express.Request, res: express.Response) {
 		return res.redirect('/course-catalogue/a-z')
 	}
 	const params = plainToInstance(CataloguePageParams, {...req.query, ...req.params})
-	console.log(params)
 	const response = await cslService.getAtoZ(params.letter, params.p, req.user)
 	const pagination = getPagination(params, response)
+	const notificationBanner = await generateNotificationBanner(req, response.results)
 	res.render('course-catalogue/a-to-z/index.njk', {
 		...params,
 		...response,
 		pagination,
+		banners: {
+			notification: notificationBanner,
+		},
 	})
 }
 
