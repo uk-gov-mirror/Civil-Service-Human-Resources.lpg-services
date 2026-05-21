1c0225a78929efc5ed6ec8c16cad2e3bf36839b4
jamie <jamie.lovell@cabinetoffice.gov.uk>
2026-05-21 15:49:24 +0100

    Lint

--- src/lib/config/index.ts
+++ src/lib/config/index.ts
@@ -3,7 +3,7 @@ import * as path from 'path'
 import 'reflect-metadata'
 import {getDayJs} from '../utils/datetime'
 
-export const  durationRegex = new RegExp(
+export const durationRegex = new RegExp(
 	'^P(?!$)(\\d+(?:\\.\\d+)?Y)?(\\d+(?:\\.\\d+)?M)?(\\d+(?:\\.\\d+)?W)?(\\d+(?:\\.\\d+)?D)?(T(?=\\d)(\\d+(?:\\.\\d+)?H)?(\\d+(?:\\.\\d+)?M)?(\\d+(?:\\.\\d+)?S)?)?$'
 )
 
@@ -168,9 +168,7 @@ export const COURSE_CATALOGUE_A_Z_MAX_COURSES = Number(env.COURSE_CATALOGUE_A_Z_
 export const POPULAR_COURSES_MAX_COURSES = Number(env.POPULAR_COURSES_MAX_COURSES) || 10
 
 const loadDuration = (val: string, fallback: string) => {
-	if (
-		!durationRegex.test(val)
-	) {
+	if (!durationRegex.test(val)) {
 		console.log(`Error formatting "${val}" to ISO 8601 duration. Falling back to ${fallback}`)
 		val = fallback
 	}
