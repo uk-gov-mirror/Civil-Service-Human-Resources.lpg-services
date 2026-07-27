f61fe883a8a0bd51c76908054626207e8eb1c3a5
jamie <jamie.lovell@cabinetoffice.gov.uk>
2026-07-27 16:44:12 +0100

    redirect to home with insufficient role

--- src/ui/controllers/nsg/controller.ts
+++ src/ui/controllers/nsg/controller.ts
@@ -3,7 +3,7 @@ import {User} from '../../../lib/model'
 
 export async function index(req: express.Request, res: express.Response) {
 	if (!(req.user as User).hasRole('LEARNING_TAG_MANAGER')) {
-		return res.redirect("/home")
+		return res.redirect('/home')
 	}
 	return res.render('nsg/index.njk')
 }
