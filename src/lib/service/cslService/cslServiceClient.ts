f896a011b7f564b8e3b26a8a353f0bbc4a4f6ca5
jamie <jamie.lovell@cabinetoffice.gov.uk>
2026-09-03 13:16:31 +0100

    PR comment

--- src/lib/service/cslService/cslServiceClient.ts
+++ src/lib/service/cslService/cslServiceClient.ts
@@ -420,21 +420,5 @@ export async function getCategoryPage(user: User, url: string, page: number, con
 		},
 		user
 	)
-	const resp = plainToInstance(CategoryPage, res)
-	// resp.courses = new BasicCourseResponse()
-	// resp.courses.page = 0
-	// resp.courses.size = 0
-	// resp.courses.results = []
-	// resp.courseCount = 24
-	// resp.linkCount = 10
-	// resp.links = new HyperlinkResponse()
-	// resp.links.page = 0
-	// resp.links.size = 10
-	// resp.links.results = [
-	// 	{title: 'Link 1', description: 'Link 1', href: 'https://link.com'},
-	// 	{title: 'Link 2', description: 'Link 2', href: 'https://link.com'},
-	// 	{title: 'Link 3', description: 'Link 3', href: 'https://link.com'},
-	// 	{title: 'Link 4', description: 'Link 4', href: 'https://link.com'},
-	// ]
-	return resp
+	return plainToInstance(CategoryPage, res)
 }
